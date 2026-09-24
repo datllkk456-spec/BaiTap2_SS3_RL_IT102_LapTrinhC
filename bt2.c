036bffdfc5951ddfb8679c0e4df8faf483cdbcb1
Nguyễn Duy Đạt <datllkk456@gmail.com>
2026-09-24 11:31:46 +0700

    commit

--- bt2.c
+++ bt2.c
@@ -1,89 +1,100 @@
 #include <stdio.h>
 
-int main() {
-    // 1. Khai bao bien
-    int tuoi, ma_phim, ma_ghe, ma_doi_tuong;
-    double gia_co_ban = 80000.0;
-    double phu_thu_ghe = 0.0;
-    double giam_gia = 0.0;
-    double gia_sau_giam = 0.0;
-    double tong_tien = 0.0;
+int main(void) {
+    int tuoi;
+    int ma_phim;
+    int ma_ghe;
+    int ma_doi_tuong;
 
-    // 2. Nhap du lieu dau vao
+    float gia_ve_goc = 80000.0;
+    float phu_thu_ghe = 0.0;
+    float giam_gia = 0.0;
+    float gia_sau_giam;
+    float tong_tien;
+
     printf("--- HE THONG BAN VE PHIM CGV ---\n");
+
+    // Nhap thong tin
     printf("Nhap tuoi khang gia: ");
     scanf("%d", &tuoi);
-    
+
     printf("Nhap ma loai phim (1-P, 2-T18): ");
     scanf("%d", &ma_phim);
-    
+
     printf("Nhap ma loai ghe (1-Thuong, 2-VIP, 3-Couple): ");
     scanf("%d", &ma_ghe);
-    
+
     printf("Nhap ma doi tuong (1-Thuong, 2-HS/SV): ");
     scanf("%d", &ma_doi_tuong);
 
-    printf("\n"); // In xuong dong cho de nhin
+    // 1. Kiem tra du lieu dau vao
+    if (tuoi <= 0 ||
+        (ma_phim != 1 && ma_phim != 2) ||
+        (ma_ghe != 1 && ma_ghe != 2 && ma_ghe != 3) ||
+        (ma_doi_tuong != 1 && ma_doi_tuong != 2)) {
 
-    // 3. Yeu cau 1: Kiem chuan du lieu dau vao (Input Validation)
-    if (tuoi <= 0 || (ma_phim != 1 && ma_phim != 2) || (ma_ghe < 1 || ma_ghe > 3) || (ma_doi_tuong != 1 && ma_doi_tuong != 2)) {
-        printf("LOI: Du lieu nhap vao khong hop le!\n");
-        return 1; // Ket thuc chuong trinh kem ma loi
+        printf("\nLOI: Du lieu nhap vao khong hop le!\n");
+        return 0;
     }
 
-    // 4. Yeu cau 2: Kiem tra quy dinh gioi han do tuoi
+    // 2. Kiem tra do tuoi phim T18
     if (ma_phim == 2 && tuoi < 18) {
-        printf("TU CHOI BAN VE: Khang gia chua du 18 tuoi de xem phim T18!\n");
-        return 0; // Tu choi ban ve va ket thuc thanh cong giao dich
+        printf("\nTU CHOI BAN VE: Khang gia chua du 18 tuoi de xem phim T18!\n");
+        return 0;
     }
 
-    // 5. Yeu cau 3: Tinh toan phu thu va chiet khau
-    
-    // Tinh phu thu ghe bang switch-case
+    // 3. Tinh phu thu ghe bang switch-case
     switch (ma_ghe) {
         case 1:
             phu_thu_ghe = 0.0;
             break;
+
         case 2:
             phu_thu_ghe = 15000.0;
             break;
+
         case 3:
             phu_thu_ghe = 30000.0;
             break;
     }
 
-    // Tinh giam gia uu dai bang if-else
+    // 4. Tinh giam gia bang if - else
     if (ma_doi_tuong == 2) {
-        giam_gia = gia_co_ban * 0.20; // Giam 20% tren gia co ban
+        giam_gia = gia_ve_goc * 0.20;
     } else {
         giam_gia = 0.0;
     }
 
-    // 6. Tong hop cong thuc tinh tien
-    gia_sau_giam = gia_co_ban - giam_gia;
+    // Tinh gia sau giam va tong tien
+    gia_sau_giam = gia_ve_goc - giam_gia;
     tong_tien = gia_sau_giam + phu_thu_ghe;
 
-    // 7. Yeu cau 4: Hien thi ket qua (Hoa don)
-    printf("--- HOA DON THANH TOAN ---\n");
-    
-    // In phan loai doi tuong
+    // 5. Xuat hoa don
+    printf("\n--- HOA DON THANH TOAN ---\n");
+
+    // Hien thi doi tuong
     if (ma_doi_tuong == 1) {
-        printf("Doi tuong: Khan gia thong thuong\n");
+        printf("Doi tuong: Khach hang thong thuong\n");
     } else {
         printf("Doi tuong: Hoc sinh / Sinh vien\n");
     }
 
-    // In phan loai ghe
-    if (ma_ghe == 1) {
-        printf("Loai ghe: Thuong\n");
-    } else if (ma_ghe == 2) {
-        printf("Loai ghe: VIP\n");
-    } else {
-        printf("Loai ghe: Couple\n");
+    // Hien thi loai ghe
+    switch (ma_ghe) {
+        case 1:
+            printf("Loai ghe: Thuong\n");
+            break;
+
+        case 2:
+            printf("Loai ghe: VIP\n");
+            break;
+
+        case 3:
+            printf("Loai ghe: Couple\n");
+            break;
     }
 
-    // In cac thong so tai chinh (ep kieu %.0f de khong in chu so thap phan)
-    printf("Gia ve goc: %.0f VND\n", gia_co_ban);
+    printf("Gia ve goc: %.0f VND\n", gia_ve_goc);
     printf("Giam gia uu dai: %.0f VND\n", giam_gia);
     printf("Phu thu ghe: %.0f VND\n", phu_thu_ghe);
     printf("Tong tien thanh toan: %.0f VND\n", tong_tien);
